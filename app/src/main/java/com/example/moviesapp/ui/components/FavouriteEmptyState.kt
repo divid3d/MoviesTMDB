1991f2437575527075cc7ccf14ed9c178362e97e
Divided <woojciechczop@gmail.com>
2022-01-20 13:04:40 +0100

    Favourites empty state color fix

---
+++ app/src/main/java/com/example/moviesapp/ui/components/FavouriteEmptyState.kt
@@ -0,0 +1,71 @@
+package com.example.moviesapp.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.moviesapp.R
+import com.example.moviesapp.model.FavouriteType
+import com.example.moviesapp.ui.theme.spacing
+
+@Composable
+fun FavouriteEmptyState(
+    modifier: Modifier,
+    type: FavouriteType,
+    onButtonClick: () -> Unit = {}
+) {
+    @StringRes
+    val infoTextResId = when (type) {
+        FavouriteType.Movie -> R.string.favourite_empty_movies_info_text
+        FavouriteType.TvSeries -> R.string.favourite_empty_tv_series_info_text
+    }
+
+    @StringRes
+    val buttonLabelResId = when (type) {
+        FavouriteType.Movie -> R.string.favourite_empty_navigate_to_movies_button_label
+        FavouriteType.TvSeries -> R.string.favourite_empty_navigate_to_tv_series_button_label
+    }
+
+    @DrawableRes
+    val icon = when (type) {
+        FavouriteType.Movie -> R.drawable.ic_outline_movie_24
+        FavouriteType.TvSeries -> R.drawable.ic_outline_tv_24
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier.size(200.dp),
+            painter = painterResource(icon),
+            contentDescription = null,
+            alpha = 0.5f,
+            colorFilter = ColorFilter.tint(Color.White),
+            contentScale = ContentScale.FillWidth
+        )
+        Spacer(modifier = Modifier.height(MaterialTheme.spacing.large))
+        Text(
+            text = stringResource(infoTextResId)
+        )
+        Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
+        OutlinedButton(onClick = onButtonClick) {
+            Text(
+                text = stringResource(buttonLabelResId)
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/moviesapp/ui/screens/favourites/FavouritesScreen.kt
+++ app/src/main/java/com/example/moviesapp/ui/screens/favourites/FavouritesScreen.kt
@@ -1,29 +1,20 @@
 package com.example.moviesapp.ui.screens.favourites
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.example.moviesapp.R
 import com.example.moviesapp.model.FavouriteType
+import com.example.moviesapp.ui.components.FavouriteEmptyState
 import com.example.moviesapp.ui.components.FavouriteTypeSelector
 import com.example.moviesapp.ui.components.PresentableGridSection
 import com.example.moviesapp.ui.screens.destinations.*
@@ -107,55 +98,6 @@ fun FavouritesScreen(
                     )
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun FavouriteEmptyState(
-    modifier: Modifier,
-    type: FavouriteType,
-    onButtonClick: () -> Unit = {}
-) {
-    @StringRes
-    val infoTextResId = when (type) {
-        FavouriteType.Movie -> R.string.favourite_empty_movies_info_text
-        FavouriteType.TvSeries -> R.string.favourite_empty_tv_series_info_text
-    }
-
-    @StringRes
-    val buttonLabelResId = when (type) {
-        FavouriteType.Movie -> R.string.favourite_empty_navigate_to_movies_button_label
-        FavouriteType.TvSeries -> R.string.favourite_empty_navigate_to_tv_series_button_label
-    }
-
-    @DrawableRes
-    val icon = when (type) {
-        FavouriteType.Movie -> R.drawable.ic_outline_movie_24
-        FavouriteType.TvSeries -> R.drawable.ic_outline_tv_24
-    }
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Image(
-            modifier = Modifier.size(200.dp),
-            painter = painterResource(icon),
-            contentDescription = null,
-            colorFilter = ColorFilter.tint(Color.White),
-            contentScale = ContentScale.FillWidth
-        )
-        Spacer(modifier = Modifier.height(MaterialTheme.spacing.large))
-        Text(
-            text = stringResource(infoTextResId)
-        )
-        Spacer(modifier = Modifier.height(MaterialTheme.spacing.medium))
-        OutlinedButton(onClick = onButtonClick) {
-            Text(
-                text = stringResource(buttonLabelResId)
-            )
         }
     }
 }
