cad9c2a739a535bf8e80dd2d9bba779ba05b7a6d
Divided <woojciechczop@gmail.com>
2022-01-31 22:18:18 +0100

    Placeholder shimmer color change

--- app/src/main/java/com/example/moviesapp/other/ComposeUtils.kt
+++ app/src/main/java/com/example/moviesapp/other/ComposeUtils.kt
@@ -16,16 +16,16 @@ import androidx.compose.ui.composed
 import androidx.compose.ui.platform.LocalDensity
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
-import com.example.moviesapp.ui.theme.White500
 import com.google.accompanist.placeholder.PlaceholderHighlight
 import com.google.accompanist.placeholder.placeholder
 import com.google.accompanist.placeholder.shimmer
 
 fun Modifier.defaultPlaceholder() = composed {
-    this.placeholder(
+    placeholder(
         visible = true,
         highlight = PlaceholderHighlight.shimmer(
-            highlightColor = White500, animationSpec = InfiniteRepeatableSpec(
+            highlightColor = MaterialTheme.colors.primary.copy(alpha = 0.5f),
+            animationSpec = InfiniteRepeatableSpec(
                 animation = tween(durationMillis = 500, easing = LinearEasing),
                 repeatMode = RepeatMode.Restart
             )
@@ -37,8 +37,13 @@ fun Modifier.defaultPlaceholder() = composed {
 
 @Composable
 fun LazyListState.isScrollingTowardsStart(): Boolean {
-    var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
-    var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
+    var previousIndex by remember(this) {
+        mutableStateOf(firstVisibleItemIndex)
+    }
+    var previousScrollOffset by remember(this) {
+        mutableStateOf(firstVisibleItemScrollOffset)
+    }
+
     return remember(this) {
         derivedStateOf {
             if (previousIndex != firstVisibleItemIndex) {
