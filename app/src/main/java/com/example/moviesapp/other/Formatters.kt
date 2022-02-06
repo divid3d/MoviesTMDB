74c779e7882426a3926357014241a72c54e7f44b
Divided <woojciechczop@gmail.com>
2022-02-06 20:34:51 +0100

    Runtime formatter fix

--- app/src/main/java/com/example/moviesapp/other/Formatters.kt
+++ app/src/main/java/com/example/moviesapp/other/Formatters.kt
@@ -22,12 +22,14 @@ fun yearRangeString(from: Date?, to: Date?): String {
 }
 
 @OptIn(ExperimentalTime::class)
-fun Int.formattedRuntime(): String {
+fun Int.formattedRuntime(): String? {
     return minutes.toComponents { hours, minutes, _, _ ->
         val hoursString = if (hours > 0) "${hours}h" else null
         val minutesString = if (minutes > 0) "${minutes}m" else null
 
-        listOfNotNull(hoursString, minutesString).joinToString(separator = " ")
+        listOfNotNull(hoursString, minutesString).run {
+            if (isEmpty()) null else joinToString(separator = " ")
+        }
     }
 }
 
