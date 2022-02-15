88f49bb5944c3bf9ffa15fd1e9dd76377512bd0c
Divided <woojciechczop@gmail.com>
2022-02-15 12:10:23 +0100

    External ids icons inner padding refactor

--- app/src/main/java/com/example/moviesapp/ui/components/ExternalIdsSection.kt
+++ app/src/main/java/com/example/moviesapp/ui/components/ExternalIdsSection.kt
@@ -4,19 +4,20 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import com.example.moviesapp.model.ExternalId
 import com.example.moviesapp.ui.theme.spacing
+import com.google.accompanist.flowlayout.FlowCrossAxisAlignment
+import com.google.accompanist.flowlayout.FlowMainAxisAlignment
+import com.google.accompanist.flowlayout.FlowRow
 
 @Composable
 fun ExternalIdsSection(
@@ -25,9 +26,12 @@ fun ExternalIdsSection(
     onExternalIdClick: (ExternalId) -> Unit = {}
 ) {
     if (externalIds.isNotEmpty()) {
-        Row(
+        FlowRow(
             modifier = modifier.wrapContentHeight(),
-            verticalAlignment = Alignment.CenterVertically
+            mainAxisAlignment = FlowMainAxisAlignment.Start,
+            crossAxisAlignment = FlowCrossAxisAlignment.Start,
+            crossAxisSpacing = MaterialTheme.spacing.small,
+            mainAxisSpacing = MaterialTheme.spacing.small
         ) {
             externalIds.map { id ->
                 IdChip(drawableRes = id.drawableRes) {
