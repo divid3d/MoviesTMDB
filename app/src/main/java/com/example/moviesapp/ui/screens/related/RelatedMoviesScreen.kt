fd2873578f0322ea3e9f4fb3f74a70d0549ac8ea
Divided <woojciechczop@gmail.com>
2022-02-28 14:14:33 +0100

    Related movies and tv series close button

--- app/src/main/java/com/example/moviesapp/ui/screens/related/RelatedMoviesScreen.kt
+++ app/src/main/java/com/example/moviesapp/ui/screens/related/RelatedMoviesScreen.kt
@@ -6,20 +6,21 @@ import android.os.Parcelable
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.moviesapp.R
 import com.example.moviesapp.model.RelationType
 import com.example.moviesapp.ui.components.AppBar
 import com.example.moviesapp.ui.components.PresentableGridSection
@@ -44,6 +45,9 @@ fun AnimatedVisibilityScope.RelatedMoviesScreen(
 ) {
     val uiState by viewModel.uiState.collectAsState()
     val onBackButtonClicked: () -> Unit = { navigator.navigateUp() }
+    val onCloseClicked: () -> Unit = {
+        navigator.popBackStack(uiState.startRoute, inclusive = false)
+    }
     val onMovieClicked: (movieId: Int) -> Unit = { id ->
         val destination = MovieDetailsScreenDestination(
             movieId = id,
@@ -56,6 +60,7 @@ fun AnimatedVisibilityScope.RelatedMoviesScreen(
     RelatedMoviesScreenContent(
         uiState = uiState,
         onBackButtonClicked = onBackButtonClicked,
+        onCloseClicked,
         onMovieClicked = onMovieClicked
     )
 }
@@ -64,14 +69,17 @@ fun AnimatedVisibilityScope.RelatedMoviesScreen(
 fun RelatedMoviesScreenContent(
     uiState: RelatedMoviesScreenUiState,
     onBackButtonClicked: () -> Unit,
+    onCloseClicked: () -> Unit,
     onMovieClicked: (movieId: Int) -> Unit
 ) {
     val movies = uiState.movies.collectAsLazyPagingItems()
 
-    val appbarTitle = when (uiState.relationType) {
-        RelationType.Similar -> "Podobne"
-        RelationType.Recommended -> "Polecane"
-    }
+    val appbarTitle = stringResource(
+        when (uiState.relationType) {
+            RelationType.Similar -> R.string.related_movies_screen_similar_appbar_label
+            RelationType.Recommended -> R.string.related_movies_screen_recommendations_appbar_label
+        }
+    )
 
     Column(
         modifier = Modifier
@@ -87,6 +95,17 @@ fun RelatedMoviesScreenContent(
                         contentDescription = "go back",
                         tint = MaterialTheme.colors.primary
                     )
+                }
+            },
+            trailing = {
+                Row(modifier = Modifier.padding(end = MaterialTheme.spacing.small)) {
+                    IconButton(onClick = onCloseClicked) {
+                        Icon(
+                            imageVector = Icons.Filled.Close,
+                            contentDescription = "close",
+                            tint = MaterialTheme.colors.primary
+                        )
+                    }
                 }
             }
         )
